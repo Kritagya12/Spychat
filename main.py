b24b303e8883bfa9e9d1ac043bb002a78e92750a
Kritagya12 <skritagya@gmail.com>
2018-03-20 18:01:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Hello!') #print is a  function in python to print whatever comes after it to the screen.
+print('Let\'s get started')
+input("What is your name?") #input is a function to input something from the spy.
+spy_name = input("What is your name?") #store the name in a variable called spy_name.
+print('Welcome ' + spy_name + '. Glad to have you back with us.') #'+' symbol to join strings together.
+spy_salutation = input("What should we call you (Mr. or Ms.)?") #Another variable to store the salutation.
+spy_salutation + " " + spy_name #We are joining the two strings together.
+spy_name = spy_salutation + " " + spy_name #Variable re-assignment
+print('Alright ' + spy_name + '. I\'d like to know a little bit more about you.')
